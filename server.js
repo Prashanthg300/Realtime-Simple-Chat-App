bc8e3c1b5ce873fb1ae0c251dadde2191245c28a
Prashanth <prashanthg300@gmail.com>
2024-06-27 11:24:14 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const http = require('http')
+const socketIo = require('socket.io')
+const path = require('path')
+
+const app = express()
+const server = http.createServer(app)
+const io = socketIo(server)
+
+app.use(express.static(path.join(__dirname)))
+
+const users = {}
+
+io.on('connection', socket => {
+  socket.on('new-user', name => {
+    users[socket.id] = name
+    socket.broadcast.emit('user-connected', name)
+  })
+  socket.on('send-chat-message', message => {
+    socket.broadcast.emit('chat-message', { message: message, name: users[socket.id] })
+  })
+  socket.on('disconnect', () => {
+    socket.broadcast.emit('user-disconnected', users[socket.id])
+    delete users[socket.id]
+  })
+})
+
+const PORT = process.env.PORT || 3000
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`)
+})
